dd4dd7583eb25ab997cd866d42a04f7092e81abf
sontb <sontb@vinuni.edu.vn>
2023-12-27 15:20:48 +0700

    test cd/ci

--- test_calculator.py
+++ test_calculator.py
@@ -1,18 +1,16 @@
-"""
-Unit tests for the calculator library
-"""
-
 import calculator
 
 
 class TestCalculator:
 
-
     def test_addition(self):
         assert 4 == calculator.add(2, 2)
-
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
 
+    def test_multiplication(self):
+        assert 2 == calculator.multiply(4, 2)
 
+    def test_division(self):
+        assert 2 == calculator.divide(4, 2)
